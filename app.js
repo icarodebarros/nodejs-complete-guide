1221948d3c99298d394316fd8150b55332d333e6
Icaro de Barros <icarodebarros@gmail.com>
2022-01-25 21:11:57 -0300

    Orders functionality

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('61f04a8cbcf32971e016d01b')
+  User.findById('61f0912a09c77e4a686abdaa')
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
