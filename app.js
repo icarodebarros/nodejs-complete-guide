2230265b92be87899cd9e23d3d4c3530acfa588d
Icaro de Barros <icarodebarros@gmail.com>
2022-01-27 18:09:55 -0300

    Better error handling

--- app.js
+++ app.js
@@ -39,32 +39,49 @@ app.use(
   })
 );
 app.use(csrfProtection);
-app.use(flash()); // Guarda informações temporárias na sessão. Utilizado aqui para mostrar aos usuário mensagens de erro/info.
+app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
-  // express passa todos os atributos do 'locals' automaticamente para todas as renderizações dos templates
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    .catch(err => {
+      next(new Error(err));
+    });
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
   .connect(MONGODB_URI)
